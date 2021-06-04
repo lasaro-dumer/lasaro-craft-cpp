1e1cc07d9306229ad0b314a34f0fc463f3856f57
Lásaro <lasaro.dumer@gmail.com>
2021-06-03 23:28:14 -0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,119 @@
+#include "raylib.h"
+#include "rlgl.h"
+#include "main.h"
+
+void DrawAxisTarget(Camera camera)
+{
+    // Center - BLACK
+    DrawSphere((Vector3) { camera.target.x, camera.target.y, camera.target.z }, 0.05f, BLACK);
+    // X-axis - RED
+    DrawLine3D((Vector3) { camera.target.x - 1.0f, camera.target.y, camera.target.z }, (Vector3) { camera.target.x + 1.0f, camera.target.y, camera.target.z }, RED);
+    DrawSphere((Vector3) { camera.target.x + 1.0f, camera.target.y, camera.target.z }, 0.05f, RED);
+    // Y-axis - GREEN
+    DrawLine3D((Vector3) { camera.target.x, camera.target.y - 1.0f, camera.target.z }, (Vector3) { camera.target.x, camera.target.y + 1.0f, camera.target.z }, GREEN);
+    DrawSphere((Vector3) { camera.target.x, camera.target.y + 1.0f, camera.target.z }, 0.05f, GREEN);
+    // Z-axis - BLUE
+    DrawLine3D((Vector3) { camera.target.x, camera.target.y, camera.target.z - 1.0f }, (Vector3) { camera.target.x, camera.target.y, camera.target.z + 1.0f }, BLUE);
+    DrawSphere((Vector3) { camera.target.x, camera.target.y, camera.target.z + 1.0f }, 0.05f, BLUE);
+}
+
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib");
+
+    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
+    Texture2D texturePack = LoadTexture("resources/texture_default.png"); // Texture loading
+    float texWidth = (float)texturePack.width;
+    float texHeight = (float)texturePack.height;
+    Rectangle frameRec = {texWidth * 1.0f / 12, texHeight * 1.0f / 8, texWidth / 12, texHeight / 8};
+
+    Camera camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    SetCameraMode(camera, CAMERA_THIRD_PERSON);
+
+    Vector3 cubePosition = {0};
+    Vector3 cubePositionGreen = {2.0f, 0.0f, 0.0f};
+    Vector3 cubePositionBlue = {-2.0f, 0.0f, 0.0f};
+    Vector3 cubePositionBlack = {0.0f, 2.0f, 0.0f};
+    float verticesLength = 1.0f;
+    float cameraMovement = 0.1f;
+    Vector3 voxelPosition = {1.0f, 1.0f, 1.0f};
+    int voxelFaces[6] = {1, 1, 1, 1, 1, 1};
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_A)){ camera.position.x += cameraMovement; camera.target.x += cameraMovement; }
+        if (IsKeyDown(KEY_D)){ camera.position.x -= cameraMovement; camera.target.x -= cameraMovement; }
+        if (IsKeyDown(KEY_S)){ camera.position.z -= cameraMovement; camera.target.z -= cameraMovement; }
+        if (IsKeyDown(KEY_W)){ camera.position.z += cameraMovement; camera.target.z += cameraMovement; }
+        if (IsKeyPressed(KEY_Q)){ camera.position.y -= 1.0f; camera.target.y -= 1.0f; }
+        if (IsKeyPressed(KEY_E)){ camera.position.y += 1.0f; camera.target.y += 1.0f; }
+        if (IsKeyPressed(KEY_KP_1)){ voxelFaces[0] = !voxelFaces[0]; }
+        if (IsKeyPressed(KEY_KP_2)){ voxelFaces[1] = !voxelFaces[1]; }
+        if (IsKeyPressed(KEY_KP_3)){ voxelFaces[2] = !voxelFaces[2]; }
+        if (IsKeyPressed(KEY_KP_4)){ voxelFaces[3] = !voxelFaces[3]; }
+        if (IsKeyPressed(KEY_KP_5)){ voxelFaces[4] = !voxelFaces[4]; }
+        if (IsKeyPressed(KEY_KP_6)){ voxelFaces[5] = !voxelFaces[5]; }
+        UpdateCamera(&camera);
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+            DrawAxisTarget(camera);
+            DrawVoxel(texturePack, frameRec, voxelPosition, 1.0f, 1.0f, 1.0f, voxelFaces, WHITE);
+            DrawCubeWires(voxelPosition, verticesLength, verticesLength, verticesLength, BLACK);
+            // DrawCube(cubePosition, verticesLength, verticesLength, verticesLength, RED);
+            // DrawCubeWires(cubePosition, verticesLength, verticesLength, verticesLength, MAROON);
+            // DrawCube(cubePositionGreen, verticesLength, verticesLength, verticesLength, GREEN);
+            // DrawCubeWires(cubePositionGreen, verticesLength, verticesLength, verticesLength, BLACK);
+            // DrawCube(cubePositionBlue, verticesLength, verticesLength, verticesLength, BLUE);
+            // DrawCubeWires(cubePositionBlue, verticesLength, verticesLength, verticesLength, BLACK);
+            // DrawCube(cubePositionBlack, verticesLength, verticesLength, verticesLength, BLACK);
+            // DrawCubeWires(cubePositionBlack, verticesLength, verticesLength, verticesLength, WHITE);
+            DrawGrid(20, 1.0f);
+            //DrawTextureRec();
+            EndMode3D();
+
+            DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
+            //Texture debugging labels
+            // DrawText(TextFormat("frameRec={%02.02f, %02.02f, %02.02f, %02.02f}", frameRec.x, frameRec.y, frameRec.height, frameRec.width), 10, 60, 20, DARKGRAY);
+            // DrawText(TextFormat("texWidth=%02.02f, texHeight=%02.02f}", texWidth, texHeight), 10, 80, 20, DARKGRAY);
+            // DrawText(TextFormat("source.x/texWidth=%02.02f, source.y/texHeight=%02.02f}", frameRec.x / texWidth, frameRec.y / texHeight), 10, 100, 20, DARKGRAY);
+            //voxel faces debugging label
+            // DrawText(TextFormat("voxelFaces: %s, %s, %s, %s, %s, %s", voxelFaces[0] ? "true" : "false", voxelFaces[1] ? "true" : "false", voxelFaces[2] ? "true" : "false",
+            //                                                           voxelFaces[3] ? "true" : "false", voxelFaces[4] ? "true" : "false", voxelFaces[5] ? "true" : "false"), 10, 120, 20, DARKGRAY);
+
+            DrawFPS(10, 10);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,131 @@
+#pragma once
+
+// NOTE: Cube position is the center position
+void DrawVoxel(Texture2D texture, Rectangle source, Vector3 position, float width, float height, float length, int faces[], Color color)
+{
+    //Start from DrawTexturePro region
+    float texWidth = (float)texture.width;
+    float texHeight = (float)texture.height;
+
+    bool flipX = false;
+
+    if (source.width < 0) { flipX = true; source.width *= -1; }
+    if (source.height < 0) source.y -= source.height;
+    //End from DrawTexturePro region
+
+    float x = position.x;
+    float y = position.y;
+    float z = position.z;
+
+    rlCheckRenderBatchLimit(36);
+
+    //rlPushMatrix();
+    // NOTE: Transformation is applied in inverse order (scale -> rotate -> translate)
+    //rlTranslatef(2.0f, 0.0f, 0.0f);
+    //rlRotatef(45, 0, 1, 0);
+    //rlScalef(2.0f, 2.0f, 2.0f);
+
+    rlBegin(RL_QUADS);
+    rlColor4ub(color.r, color.g, color.b, color.a);
+    // Front Face
+    rlSetTexture(texture.id);
+    if (faces[0]) {
+        rlNormal3f(0.0f, 0.0f, 1.0f);                  // Normal Pointing Towards Viewer
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x - width / 2, y - height / 2, z + length / 2);  // Bottom Left Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x + width / 2, y - height / 2, z + length / 2);  // Bottom Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        rlVertex3f(x + width / 2, y + height / 2, z + length / 2);  // Top Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        else rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        rlVertex3f(x - width / 2, y + height / 2, z + length / 2);  // Top Left Of The Texture and Quad
+    }
+    // Back Face
+    if (faces[1]) {
+        rlNormal3f(0.0f, 0.0f, -1.0f);                  // Normal Pointing Away From Viewer
+        if (flipX) rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x - width / 2, y - height / 2, z - length / 2);  // Bottom Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        rlVertex3f(x - width / 2, y + height / 2, z - length / 2);  // Top Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        else rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        rlVertex3f(x + width / 2, y + height / 2, z - length / 2);  // Top Left Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x + width / 2, y - height / 2, z - length / 2);  // Bottom Left Of The Texture and Quad
+    }
+    // Top Face
+    if (faces[2]) {
+        rlNormal3f(0.0f, 1.0f, 0.0f);                  // Normal Pointing Up
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        else rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        rlVertex3f(x - width / 2, y + height / 2, z - length / 2);  // Top Left Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x - width / 2, y + height / 2, z + length / 2);  // Bottom Left Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x + width / 2, y + height / 2, z + length / 2);  // Bottom Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        rlVertex3f(x + width / 2, y + height / 2, z - length / 2);  // Top Right Of The Texture and Quad
+    }
+    // Bottom Face
+    if (faces[3]) {
+        rlNormal3f(0.0f, -1.0f, 0.0f);                  // Normal Pointing Down
+        if (flipX) rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        rlVertex3f(x - width / 2, y - height / 2, z - length / 2);  // Top Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        else rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        rlVertex3f(x + width / 2, y - height / 2, z - length / 2);  // Top Left Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x + width / 2, y - height / 2, z + length / 2);  // Bottom Left Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x - width / 2, y - height / 2, z + length / 2);  // Bottom Right Of The Texture and Quad
+    }
+    // Right face
+    if (faces[4]) {
+        rlNormal3f(1.0f, 0.0f, 0.0f);                  // Normal Pointing Right
+        if (flipX) rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x + width / 2, y - height / 2, z - length / 2);  // Bottom Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        rlVertex3f(x + width / 2, y + height / 2, z - length / 2);  // Top Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        else rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        rlVertex3f(x + width / 2, y + height / 2, z + length / 2);  // Top Left Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x + width / 2, y - height / 2, z + length / 2);  // Bottom Left Of The Texture and Quad
+    }
+    // Left Face
+    if (faces[5]) {
+        rlNormal3f(-1.0f, 0.0f, 0.0f);                  // Normal Pointing Left
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x - width / 2, y - height / 2, z - length / 2);  // Bottom Left Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+        rlVertex3f(x - width / 2, y - height / 2, z + length / 2);  // Bottom Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        else rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        rlVertex3f(x - width / 2, y + height / 2, z + length / 2);  // Top Right Of The Texture and Quad
+        if (flipX) rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+        else rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+        rlVertex3f(x - width / 2, y + height / 2, z - length / 2);  // Top Left Of The Texture and Quad
+    }
+    rlEnd();
+    //rlPopMatrix();
+
+    rlSetTexture(0);
+}
